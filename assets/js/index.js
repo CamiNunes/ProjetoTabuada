8a3436671c72957dc9bce6d07a929dec5e431fcd
CamiNunes <cvn.camila@gmail.com>
2020-02-10 00:07:58 -0300

    Trabalhando com função no JavaScript;

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,33 @@
-let tabuada = document.querySelector("#tabuada tbody");
-let valorA = 9;
+function calculaTabuada(){
 
-tabuada.innerHTML = '';
+    let tabuada = document.querySelector("#tabuada tbody");
+    let valorA = parseInt(document.querySelector("#valorA").value);
 
-for(let valorB = 0; valorB <= 10; valorB++){
+    tabuada.innerHTML = '';
 
-    let resultado = valorA * valorB;
+    for(let valorB = 0; valorB <= 10; valorB++){
 
-    let template = `
-        <td>${valorA}</td>
-        <td>x</td>
-        <td>${valorB}</td>
-        <td>=</td>
-        <td>${resultado}</td>
-    `;
+        let resultado = valorA * valorB;
 
-    let tr = document.createElement('tr');
+        let template = `
+            <td>${valorA}</td>
+            <td>x</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+        `;
 
-    tr.innerHTML = template;
+        let tr = document.createElement('tr');
 
-    tabuada.append(tr);
+        tr.innerHTML = template;
+
+        tabuada.append(tr);
+
+    }
 
 }
+
+calculaTabuada();
+
+document.querySelector("#valorA").addEventListener('change', calculaTabuada);
 
